43b5d29faae9bb7b3a90e8b17bd6d7698b9da79e
Alistair Bill <alistair.bill@gmail.com>
2018-11-03 22:10:09 +0000

    Add current progress

--- main/infrared_nec_main.c
+++ main/infrared_nec_main.c
@@ -1,4 +1,4 @@
-/* NEC remote infrared RMT example
+/* RF remote RMT example
 
    This example code is in the Public Domain (or CC0 licensed, at your option.)
 
@@ -18,51 +18,32 @@
 #include "driver/periph_ctrl.h"
 #include "soc/rmt_reg.h"
 
-static const char* NEC_TAG = "NEC";
+static const char* RMT_TAG = "RMT";
 
-//CHOOSE SELF TEST OR NORMAL TEST
-#define RMT_RX_SELF_TEST   1
-
-/******************************************************/
-/*****                SELF TEST:                  *****/
-/*Connect RMT_TX_GPIO_NUM with RMT_RX_GPIO_NUM        */
-/*TX task will send NEC data with carrier disabled    */
-/*RX task will print NEC data it receives.            */
-/******************************************************/
-#if RMT_RX_SELF_TEST
-#define RMT_RX_ACTIVE_LEVEL  1   /*!< Data bit is active high for self test mode */
-#define RMT_TX_CARRIER_EN    0   /*!< Disable carrier for self test mode  */
-#else
-//Test with infrared LED, we have to enable carrier for transmitter
-//When testing via IR led, the receiver waveform is usually active-low.
-#define RMT_RX_ACTIVE_LEVEL  0   /*!< If we connect with a IR receiver, the data is active low */
-#define RMT_TX_CARRIER_EN    1   /*!< Enable carrier for IR transmitter test with IR led */
-#endif
-
-#define RMT_TX_CHANNEL    1     /*!< RMT channel for transmitter */
-#define RMT_TX_GPIO_NUM  14     /*!< GPIO number for transmitter signal */
-#define RMT_RX_CHANNEL    0     /*!< RMT channel for receiver */
-#define RMT_RX_GPIO_NUM  15     /*!< GPIO number for receiver */
-#define RMT_CLK_DIV      100    /*!< RMT counter clock divider */
-#define RMT_TICK_10_US    (80000000/RMT_CLK_DIV/100000)   /*!< RMT counter value for 10 us.(Source clock is APB clock) */
+#define RMT_TX_GPIO_NUM 14 // Transmit GPIO pin
+#define RMT_RX_GPIO_NUM 15 // Receive GPIO pin
+#define RMT_CLK_DIV 100 // RMT counter clock divider
+#define RMT_TICK_10_US (80000000/RMT_CLK_DIV/100000) // RMT counter value for 10 us (source clock is APB clock)
 
 #define PULSE_LENGTH_US 350
-#define NEC_HEADER_HIGH_US    1 * PULSE_LENGTH_US                         /*!< NEC protocol header: positive 9ms */
-#define NEC_HEADER_LOW_US     31 * PULSE_LENGTH_US                         /*!< NEC protocol header: negative 4.5ms*/
-#define NEC_BIT_ONE_HIGH_US    3 * PULSE_LENGTH_US                         /*!< NEC protocol data bit 1: positive 0.56ms */
-#define NEC_BIT_ONE_LOW_US    1 * PULSE_LENGTH_US  /*!< NEC protocol data bit 1: negative 1.69ms */
-#define NEC_BIT_ZERO_HIGH_US   1 * PULSE_LENGTH_US                         /*!< NEC protocol data bit 0: positive 0.56ms */
-#define NEC_BIT_ZERO_LOW_US   3 * PULSE_LENGTH_US  /*!< NEC protocol data bit 0: negative 0.56ms */
-#define NEC_BIT_MARGIN         20                          /*!< NEC parse margin time */
+#define HEADER_HIGH_US (1 * PULSE_LENGTH_US) // Sync bit: 1 pulse high
+#define HEADER_LOW_US (31 * PULSE_LENGTH_US) // Sync bit: 31 pulses low
+#define BIT_ONE_HIGH_US (3 * PULSE_LENGTH_US) // Bit 1: 3 pulses high
+#define BIT_ONE_LOW_US (1 * PULSE_LENGTH_US) // Bit 1: 1 pulse low
+#define BIT_ZERO_HIGH_US (1 * PULSE_LENGTH_US) // Bit 0: 1 pulse high
+#define BIT_ZERO_LOW_US (3 * PULSE_LENGTH_US) // Bit 0: 3 pulses low
+#define BIT_MARGIN 20 // Parse margin time
 
-#define NEC_ITEM_DURATION(d)  ((d & 0x7fff)*10/RMT_TICK_10_US)  /*!< Parse duration time from memory register value */
-#define NEC_DATA_ITEM_NUM   26  /*!< NEC code item number: header + 32bit data + end */
-#define rmt_item32_tIMEOUT_US  15000   /*!< RMT receiver timeout value(us) */
+#define ITEM_DURATION(d)  ((d & 0x7fff)*10/RMT_TICK_10_US) // Parse duration time from memory register value
+#define DATA_ITEM_NUM 25  // number of bits: header + 24bit data
+#define rmt_item32_tIMEOUT_US 15000 // RMT receiver timeout value (us)
+
+#define TX_REPEATS 1
 
 /*
- * @brief Build register value of waveform for NEC one data bit
+ * @brief Build register value of waveform for one data bit
  */
-static inline void nec_fill_item_level(rmt_item32_t* item, int high_us, int low_us)
+static inline void fill_item_level(rmt_item32_t* item, int high_us, int low_us)
 {
     item->level0 = 1;
     item->duration0 = (high_us) / 10 * RMT_TICK_10_US;
@@ -71,44 +52,36 @@ static inline void nec_fill_item_level(rmt_item32_t* item, int high_us, int low_
 }
 
 /*
- * @brief Generate NEC header value: active 9ms + negative 4.5ms
+ * @brief Generate sync bit: 1 pulse high + 31 pulses low
  */
-static void nec_fill_item_header(rmt_item32_t* item)
+static void fill_item_header(rmt_item32_t* item)
 {
-    nec_fill_item_level(item, NEC_HEADER_HIGH_US, NEC_HEADER_LOW_US);
+    fill_item_level(item, HEADER_HIGH_US, HEADER_LOW_US);
 }
 
 /*
- * @brief Generate NEC data bit 1: positive 0.56ms + negative 1.69ms
+ * @brief Generate data bit 1: 3 pulses high + 1 pulse low
  */
-static void nec_fill_item_bit_one(rmt_item32_t* item)
+static void fill_item_bit_one(rmt_item32_t* item)
 {
-    nec_fill_item_level(item, NEC_BIT_ONE_HIGH_US, NEC_BIT_ONE_LOW_US);
+    fill_item_level(item, BIT_ONE_HIGH_US, BIT_ONE_LOW_US);
 }
 
 /*
- * @brief Generate NEC data bit 0: positive 0.56ms + negative 0.56ms
+ * @brief Generate data bit 0: 1 pulse high + 3 pulses low
  */
-static void nec_fill_item_bit_zero(rmt_item32_t* item)
+static void fill_item_bit_zero(rmt_item32_t* item)
 {
-    nec_fill_item_level(item, NEC_BIT_ZERO_HIGH_US, NEC_BIT_ZERO_LOW_US);
-}
-
-/*
- * @brief Generate NEC end signal: positive 0.56ms
- */
-static void nec_fill_item_end(rmt_item32_t* item)
-{
-    nec_fill_item_level(item, 0, 0);
+    fill_item_level(item, BIT_ZERO_HIGH_US, BIT_ZERO_LOW_US);
 }
 
 /*
  * @brief Check whether duration is around target_us
  */
-inline bool nec_check_in_range(int duration_ticks, int target_us, int margin_us)
+inline bool check_in_range(int duration_ticks, int target_us, int margin_us)
 {
-    if(( NEC_ITEM_DURATION(duration_ticks) < (target_us + margin_us))
-        && ( NEC_ITEM_DURATION(duration_ticks) > (target_us - margin_us))) {
+    if(( ITEM_DURATION(duration_ticks) < (target_us + margin_us))
+        && ( ITEM_DURATION(duration_ticks) > (target_us - margin_us))) {
         return true;
     } else {
         return false;
@@ -116,26 +89,26 @@ inline bool nec_check_in_range(int duration_ticks, int target_us, int margin_us)
 }
 
 /*
- * @brief Check whether this value represents an NEC header
+ * @brief Check whether this value represents a sync bit
  */
-static bool nec_header_if(rmt_item32_t* item)
+static bool header_if(rmt_item32_t* item)
 {
-    if((item->level0 == RMT_RX_ACTIVE_LEVEL && item->level1 != RMT_RX_ACTIVE_LEVEL)
-        && nec_check_in_range(item->duration0, NEC_HEADER_HIGH_US, NEC_BIT_MARGIN)
-        && nec_check_in_range(item->duration1, NEC_HEADER_LOW_US, NEC_BIT_MARGIN)) {
+    if((item->level0 == 1 && item->level1 == 0)
+        && check_in_range(item->duration0, HEADER_HIGH_US, BIT_MARGIN)
+        && check_in_range(item->duration1, HEADER_LOW_US, BIT_MARGIN)) {
         return true;
     }
     return false;
 }
 
 /*
- * @brief Check whether this value represents an NEC data bit 1
+ * @brief Check whether this value represents a data bit 1
  */
-static bool nec_bit_one_if(rmt_item32_t* item)
+static bool bit_one_if(rmt_item32_t* item)
 {
-    if((item->level0 == RMT_RX_ACTIVE_LEVEL && item->level1 != RMT_RX_ACTIVE_LEVEL)
-        && nec_check_in_range(item->duration0, NEC_BIT_ONE_HIGH_US, NEC_BIT_MARGIN)
-        && (nec_check_in_range(item->duration1, NEC_BIT_ONE_LOW_US, NEC_BIT_MARGIN)
+    if((item->level0 == 1 && item->level1 == 0)
+        && check_in_range(item->duration0, BIT_ONE_HIGH_US, BIT_MARGIN)
+        && (check_in_range(item->duration1, BIT_ONE_LOW_US, BIT_MARGIN)
             || item->duration1 == 0)) {
         return true;
     }
@@ -143,13 +116,13 @@ static bool nec_bit_one_if(rmt_item32_t* item)
 }
 
 /*
- * @brief Check whether this value represents an NEC data bit 0
+ * @brief Check whether this value represents a data bit 0
  */
-static bool nec_bit_zero_if(rmt_item32_t* item)
+static bool bit_zero_if(rmt_item32_t* item)
 {
-    if((item->level0 == RMT_RX_ACTIVE_LEVEL && item->level1 != RMT_RX_ACTIVE_LEVEL)
-        && nec_check_in_range(item->duration0, NEC_BIT_ZERO_HIGH_US, NEC_BIT_MARGIN)
-        && (nec_check_in_range(item->duration1, NEC_BIT_ZERO_LOW_US, NEC_BIT_MARGIN)
+    if((item->level0 == 1 && item->level1 == 0)
+        && check_in_range(item->duration0, BIT_ZERO_HIGH_US, BIT_MARGIN)
+        && (check_in_range(item->duration1, BIT_ZERO_LOW_US, BIT_MARGIN)
             || item->duration1 == 0)) {
         return true;
     }
@@ -158,35 +131,23 @@ static bool nec_bit_zero_if(rmt_item32_t* item)
 
 
 /*
- * @brief Parse NEC 32 bit waveform to address and command.
+ * @brief Parse 25 bit waveform to data
  */
-static int nec_parse_items(rmt_item32_t* item, int item_num, uint32_t* addr, uint8_t* data)
+static int parse_items(rmt_item32_t* item, int item_num, uint32_t* data)
 {
     int w_len = item_num;
-    if(w_len < NEC_DATA_ITEM_NUM - 1) {
+    if(w_len < DATA_ITEM_NUM) {
         return -1;
     }
     int i = 0, j = 0;
-    if(!nec_header_if(item++)) {
+    if(!header_if(item++)) {
         return -1;
     }
-    uint32_t addr_t = 0;
-    for(j = 0; j < 20; j++) {
-        if(nec_bit_one_if(item)) {
-            addr_t |= (1 << j);
-        } else if(nec_bit_zero_if(item)) {
-            addr_t |= (0 << j);
-        } else {
-            return -1;
-        }
-        item++;
-        i++;
-    }
-    uint8_t data_t = 0;
-    for(j = 0; j < 4; j++) {
-        if(nec_bit_one_if(item)) {
+    uint32_t data_t = 0;
+    for(j = 0; j < 24; j++) {
+        if(bit_one_if(item)) {
             data_t |= (1 << j);
-        } else if(nec_bit_zero_if(item)) {
+        } else if(bit_zero_if(item)) {
             data_t |= (0 << j);
         } else {
             return -1;
@@ -194,71 +155,58 @@ static int nec_parse_items(rmt_item32_t* item, int item_num, uint32_t* addr, uin
         item++;
         i++;
     }
-    *addr = addr_t;
     *data = data_t;
     return i;
 }
 
 /*
- * @brief Build NEC 32bit waveform.
+ * @brief Build 25bit waveform.
  */
-static int nec_build_items(int channel, rmt_item32_t* item, int item_num, uint32_t addr, uint8_t cmd_data)
+static int build_items(rmt_item32_t* item, int item_num, uint32_t data)
 {
     int i = 0, j = 0;
-    if(item_num < NEC_DATA_ITEM_NUM) {
+    if(item_num < DATA_ITEM_NUM) {
         return -1;
     }
-    nec_fill_item_header(item++);
+    fill_item_header(item++);
     i++;
-    for(j = 0; j < 20; j++) {
-        if(addr & 0x1) {
-            nec_fill_item_bit_one(item);
+    for(j = 0; j < 24; j++) {
+        if(data & 0x1) {
+            fill_item_bit_one(item);
         } else {
-            nec_fill_item_bit_zero(item);
+            fill_item_bit_zero(item);
         }
         item++;
         i++;
-        addr >>= 1;
+        data >>= 1;
     }
-    for(j = 0; j < 4; j++) {
-        if(cmd_data & 0x1) {
-            nec_fill_item_bit_one(item);
-        } else {
-            nec_fill_item_bit_zero(item);
-        }
-        item++;
-        i++;
-        cmd_data >>= 1;
-    }
-    nec_fill_item_end(item);
-    i++;
     return i;
 }
 
 static void print_rx_buffer(rmt_item32_t* item, int item_num)
 {
     for (int i = 0; i < item_num; i++) {
-        ESP_LOGI(NEC_TAG, "%d %d %d %d", item[i].level0, item[i].duration0, item[i].level1, item[i].duration1);
+        ESP_LOGI(RMT_TAG, "%d %d %d %d", item[i].level0, item[i].duration0, item[i].level1, item[i].duration1);
     }
 }
 
 /*
  * @brief RMT transmitter initialization
  */
-static void nec_tx_init()
+static void tx_init()
 {
     rmt_config_t rmt_tx;
-    rmt_tx.channel = RMT_TX_CHANNEL;
+    rmt_tx.channel = 1;
     rmt_tx.gpio_num = RMT_TX_GPIO_NUM;
     rmt_tx.mem_block_num = 1;
     rmt_tx.clk_div = RMT_CLK_DIV;
     rmt_tx.tx_config.loop_en = false;
-    rmt_tx.tx_config.carrier_duty_percent = 50;
+    rmt_tx.tx_config.carrier_duty_percent = 0;
     rmt_tx.tx_config.carrier_freq_hz = 38000;
     rmt_tx.tx_config.carrier_level = 1;
-    rmt_tx.tx_config.carrier_en = RMT_TX_CARRIER_EN;
+    rmt_tx.tx_config.carrier_en = 0;
     rmt_tx.tx_config.idle_level = 0;
-    rmt_tx.tx_config.idle_output_en = true;
+    rmt_tx.tx_config.idle_output_en = false;
     rmt_tx.rmt_mode = 0;
     rmt_config(&rmt_tx);
     rmt_driver_install(rmt_tx.channel, 0, 0);
@@ -267,10 +215,10 @@ static void nec_tx_init()
 /*
  * @brief RMT receiver initialization
  */
-static void nec_rx_init()
+static void rx_init()
 {
     rmt_config_t rmt_rx;
-    rmt_rx.channel = RMT_RX_CHANNEL;
+    rmt_rx.channel = 0;
     rmt_rx.gpio_num = RMT_RX_GPIO_NUM;
     rmt_rx.clk_div = RMT_CLK_DIV;
     rmt_rx.mem_block_num = 1;
@@ -283,13 +231,13 @@ static void nec_rx_init()
 }
 
 /**
- * @brief RMT receiver demo, this task will print each received NEC data.
+ * @brief RMT receiver demo, this task will print each received data.
  *
  */
 static void rmt_example_nec_rx_task()
 {
-    int channel = RMT_RX_CHANNEL;
-    nec_rx_init();
+    int channel = 0; // RMT channel for receiver
+    rx_init();
     RingbufHandle_t rb = NULL;
     //get RMT RX ringbuffer
     rmt_get_ringbuf_handle(channel, &rb);
@@ -301,12 +249,17 @@ static void rmt_example_nec_rx_task()
         //We just need to parse the value and return the spaces of ringbuffer.
         rmt_item32_t* item = (rmt_item32_t*) xRingbufferReceive(rb, &rx_size, 1000);
         if(item) {
-            uint32_t rmt_addr;
-            uint8_t rmt_cmd;
+            uint32_t rmt_data;
+            int offset = 0;
             int num_items = rx_size / sizeof(rmt_item32_t);
-            int rec = nec_parse_items(item, num_items, &rmt_addr, &rmt_cmd);
-            if (rec > 0) {
-                ESP_LOGI(NEC_TAG, "addr: 0x%04x cmd: 0x%04x", rmt_addr, rmt_cmd);
+            while (1) {
+                int res = parse_items(item + offset, num_items - offset, &rmt_data);
+                if (res > 0) {
+                    offset += res + 1;
+                    ESP_LOGI(RMT_TAG, "data: 0x%06x", rmt_data);
+                } else {
+                    break;
+                }
             }
             //after parsing the data, return spaces to ringbuffer.
             vRingbufferReturnItem(rb, (void*) item);
@@ -318,24 +271,31 @@ static void rmt_example_nec_rx_task()
 }
 
 /**
- * @brief RMT transmitter demo, this task will periodically send NEC data. (100 * 32 bits each time.)
+ * @brief RMT transmitter demo, this task will periodically send data. (25 bits each time)
  *
  */
 static void rmt_example_nec_tx_task()
 {
     vTaskDelay(10);
-    nec_tx_init();
-    esp_log_level_set(NEC_TAG, ESP_LOG_INFO);
-    int channel = RMT_TX_CHANNEL;
-    uint8_t cmd = 0xa;
-    uint32_t addr = 0xffff;
+    tx_init();
+    esp_log_level_set(RMT_TAG, ESP_LOG_INFO);
+    int channel = 1; // RMT TX channel
+    uint32_t data = 0xabcdef;
+    int tx_num = TX_REPEATS;
     while(1) {
-        ESP_LOGI(NEC_TAG, "RMT TX DATA");
-        size_t size = (sizeof(rmt_item32_t) * NEC_DATA_ITEM_NUM);
+        ESP_LOGI(RMT_TAG, "RMT TX DATA");
+        size_t size = (sizeof(rmt_item32_t) * DATA_ITEM_NUM * tx_num);
         rmt_item32_t* item = (rmt_item32_t*) malloc(size);
-        int item_num = NEC_DATA_ITEM_NUM;
+        int item_num = DATA_ITEM_NUM * tx_num;
         memset((void*) item, 0, size);
-        nec_build_items(channel, item, item_num, addr & 0xfffff, cmd & 0xf);
+        int i, offset = 0;
+        while (1) {
+            i = build_items(item + offset, item_num - offset, data);
+            if (i < 0) {
+                break;
+            }
+            offset += i;
+        }
         //To send data according to the waveform items.
         rmt_write_items(channel, item, item_num, true);
         //Wait until sending is done.
